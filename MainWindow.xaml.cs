687d8c7488ebb7966c89bc62fc0e110e79a3582f
Washington George <Georgeodin@outlook.com>
2020-04-23 11:05:38 +0800

    第一次提交

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HexoTool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Setting s;
+        private string Editor="";
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitConfig();
+            Loadscaffolds();
+        }
+        private void Window_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+        private async void Setting_Click(object sender, RoutedEventArgs e)
+        {
+            s = new Setting();
+            _ = await DialogHost.Show(s, "RootDialogHost");
+            Editor = s.select.Content.ToString();
+        }
+        private void InitConfig()
+        {
+            
+            if(ConfigurationManager.AppSettings["MarkDownEditer"] != null)
+            {
+                Editor = ConfigurationManager.AppSettings["MarkDownEditer"].ToString();
+            }
+                
+        }
+        private string ExcuteCMD(string Command)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/S /C " + Command)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true
+            };
+
+            var process = new Process { StartInfo = processInfo };
+            process.Start();
+            string output= process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output.ToString();
+        }
+
+        private void UpLoad_Click(object sender, RoutedEventArgs e)
+        {
+            sbar.IsActive = false;
+            sbarmessage.Content = "正在发布,请稍后";
+            sbar.IsActive = true;
+            try
+            {
+                ExcuteCMD("hexo clean");
+                ExcuteCMD("hexo g");
+                ExcuteCMD("hexo d");
+            }
+            catch
+            {
+                sbar.IsActive = false;
+                sbarmessage.Content = "找不到hexo";
+                sbar.IsActive = true;
+            }
+            
+            string output=ExcuteCMD("python ./libs/ReloadGitee.py");
+                sbar.IsActive = false;
+                sbarmessage.Content = "执行完毕,返回信息如下:  "+output;
+                sbar.IsActive = true;
+            
+        }
+
+        private void Newartical_Click(object sender, RoutedEventArgs e)
+        {
+            if (Editor == "")
+            {
+                sbarmessage.Content = "请先选择MarkDown编辑器";
+                sbar.IsActive = true;
+                return;
+            }
+            else
+            {
+                ExcuteCMD("hexo new " + sc.SelectedItem.ToString() + " " + Filenamebox.Text);
+                ExcuteCMD(Editor + " " + "./source/_posts/" + Filenamebox.Text+".md");
+            }
+        }
+        private void Loadscaffolds()
+        {
+            
+            if(Directory.Exists(Directory.GetCurrentDirectory().Replace("\\", "/") + "/scaffolds"))
+            {
+                DirectoryInfo TheFolder = new DirectoryInfo(Directory.GetCurrentDirectory().Replace("\\", "/") + "/scaffolds");
+                foreach (FileInfo NextFolder in TheFolder.GetFiles())
+                {
+                    sc.Items.Add(NextFolder.Name.Split('.')[0]);
+                }
+            }
+            else
+            {
+                sbarmessage.Content = "请将本工具放置于Hexo目录下";
+                sbar.IsActive = true;
+            }
+            
+        }
+
+        private void sbarmessage_ActionClick(object sender, RoutedEventArgs e)
+        {
+            sbar.IsActive = false;
+        }
+    }
+}
---
+++ Setting.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace HexoTool
+{
+    /// <summary>
+    /// Setting.xaml 的交互逻辑
+    /// </summary>
+    public partial class Setting : UserControl
+    {
+        private string filename;
+        public Setting()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择编辑器";
+            dialog.Filter = "配置文件(*.exe)|*.exe";
+            if (dialog.ShowDialog() == true)
+            {
+                filename = dialog.FileName.Replace("\\", "/");
+                select.Content = filename;
+                if (ConfigurationManager.AppSettings["MarkDownEditer"] != null)
+                {
+                    ConfigurationManager.AppSettings.Remove("MarkDownEditer");
+                }
+                System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings.Add("MarkDownEditer", filename);
+                config.Save();
+            }
+        }
+    }
+}
